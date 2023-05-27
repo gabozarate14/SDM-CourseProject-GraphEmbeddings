a3e06e14f05a2f754fc29718f6efef7852e7660d
gabozarate14 <gabozarate14@gmail.com>
2023-05-27 18:21:19 +0200

    proof of concept

--- recommender_application.py
+++ recommender_application.py
@@ -6,7 +6,6 @@ from tensorflow.keras.models import load_model
 import pandas as pd
 from datetime import datetime
 
-
 from collections import Counter
 
 DATABASE_URL = f'bolt://localhost:7687'
@@ -72,23 +71,35 @@ for node in G.nodes():
 node2vec = Node2Vec(G, dimensions=128, walk_length=80, num_walks=10, p=1, q=1)
 model = node2vec.fit(window=10, min_count=1)
 
+# Prepare the interactions
+papers = []
+reviewers = []
+for paper in paper_graph_dict.keys():
+    for reviewer in rev_graph_dict.keys():
+        papers.append(paper)
+        reviewers.append(reviewer)
+
 # Prepare the data using the embeddings
-X_reviewer = np.array([model.wv[node] for node in rev_graph_dict.keys()])
-X_paper = np.array([model.wv[node] for node in paper_graph_dict.keys()])
+X_reviewer = np.array([model.wv[node] for node in reviewers])
+X_paper = np.array([model.wv[node] for node in papers])
 
 # Do the predictions
 loaded_model = load_model('model/reviewer_recommender.h5')
 predictions = loaded_model.predict([X_reviewer, X_paper])
 predictions = np.round(predictions).flatten()
 
-df_predictions = pd.DataFrame({'Paper': paper_graph_dict.values(),
-                               'Reviewer': rev_graph_dict.values(),
+df_predictions = pd.DataFrame({'Paper': papers,
+                               'Reviewer': reviewers,
                                'Recommended': predictions})
 
+df_predictions['Paper'] = df_predictions['Paper'].apply(lambda x: paper_graph_dict[x])
+df_predictions['Reviewer'] = df_predictions['Reviewer'].apply(lambda x: rev_graph_dict[x])
 df_predictions['Recommended'] = df_predictions['Recommended'].apply(lambda x: 'Yes' if x == 1.0 else 'No')
+
+# Select only recommended reviewers
+df_predictions = df_predictions[df_predictions['Recommended'] == 'Yes']
 
 # Export DataFrame to CSV
 current_time = datetime.now().strftime("%d%m%YT%H%M%S")
 output_filename = f'recommendations/predictions_{current_time}.csv'
 df_predictions.to_csv(output_filename, index=False)
-
